4374a5b3c18f1ef30f32c9a34272271ecb56a5e0
Marcel Rocha Fonteles Vieira <marcelvieira@infarma.com.br>
2019-09-07 21:26:25 -0300

    configuring database

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'blog_development' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'wordpress' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'password' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
